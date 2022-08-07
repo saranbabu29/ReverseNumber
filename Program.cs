bc59811fad42b2013258adbf7ccc15eae4a389a8
saranbabu29 <sharanraj982@gmail.com>
2022-08-07 15:22:24 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, rem, result = 0;
+            Console.WriteLine("Enter the Number ");
+            number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                rem = number % 10;
+                result = result * 10 + rem;
+                number = number / 10;
+            }
+            Console.WriteLine("The reverse number is " + result);
+        }
+    }
+}
